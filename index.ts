383e8982d11704733533bb81abf41c325c919f20
Yasushi Ikeda <yikeda6616@gmail.com>
2019-08-24 02:27:30 +0900

    Add login function

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
+import * as dotenv from "dotenv";
+
 import ig from "./instagram";
+
+const env = { ...(dotenv.config().parsed as any) };
 
 (async () => {
   await ig.initialize();
+
+  await ig.login(env.USERNAME, env.PASSWORD);
+
+  debugger;
 })();
--- instagram.ts
+++ instagram.ts
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 
-const BASE_URL = "https://instagram.com/";
+const BASE_URL =
+  "https://www.instagram.com/accounts/login/?source=auth_switcher";
 
 const instagram = {
   browser: null as any,
@@ -13,10 +14,37 @@ const instagram = {
     });
 
     instagram.page = await instagram.browser.newPage();
+  },
 
+  // TODO: LOGIN function
+  login: async (username: string, password: string) => {
     await instagram.page.goto(BASE_URL, { waitUntil: "networkidle2" });
 
-    await instagram.browser.close();
+    // let loginButton = await instagram.page.$x(
+    //   '//a[contains(text(), "Log in")]'
+    // );
+
+    // // Click on the login url button
+    // await loginButton[0].click();
+
+    // await instagram.page.waitForNavigation({ waitUntil: "networkidle2" });
+
+    // await instagram.page.waitFor(1000);
+
+    // Type input fields - User ID & Password
+    await instagram.page.type('input[name="username"]', username, {
+      delay: 50,
+    });
+    await instagram.page.type('input[name="password"]', password, {
+      delay: 50,
+    });
+
+    // Click on the login button
+    const loginButton = await instagram.page.$('button[type="submit"]');
+
+    await loginButton.click();
+
+    await instagram.page.waitForNavigation({ waitUntil: "networkidle2" });
   },
 };
 
