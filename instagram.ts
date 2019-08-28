35d2ec82a206dcfcbf2c56f342ec2612211da485
Yasushi Ikeda <yikeda6616@gmail.com>
2019-08-28 15:17:46 +0900

    Update let to const

--- instagram.ts
+++ instagram.ts
@@ -42,12 +42,12 @@ const instagram = {
       await instagram.page.goto(TAG_URL(tag), { waitUntil: "networkidle2" });
       await instagram.page.waitFor(1000);
 
-      let posts = await instagram.page.$$(
+      const posts = await instagram.page.$$(
         'article > div:nth-child(3) img[decoding="auto"]'
       );
 
       for (let i = 0; i < 3; i++) {
-        let post = posts[i];
+        const post = posts[i];
 
         await post.click();
 
@@ -56,13 +56,19 @@ const instagram = {
         );
         await instagram.page.waitFor(1000);
 
-        let isLikable = await post.$('span[aria-label="Like"]');
+        const isLikable = await post.$('span[aria-label="Like"]');
 
         if (isLikable) {
-          await post.click('span[aria-label="Like"]');
+          await instagram.page.click('span[aria-label="Like"]');
         }
 
-        await instagram.page.click('//button[contains(text(), "Close")]');
+        const closeModalButton = await instagram.page.$x(
+          '//button[contains(text(), "Close")]'
+        );
+
+        await closeModalButton[0].click();
+
+        await instagram.page.waitFor(1000);
       }
     }
   },
